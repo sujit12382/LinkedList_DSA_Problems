143e80d29f180434f1caa69f5062e282bf5ee0c5
sujit12382 <117746211+sujit12382@users.noreply.github.com>
2024-08-15 11:57:34 +0530

    Create add 1 to LL.cpp
    You are given a linked list where each element in the list is a node and have an integer data. You need to add 1 to the number formed by concatinating all the list node numbers together and return the head of the modified linked list.

---
+++ addition_to_LL.cpp
@@ -0,0 +1,131 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data % 10;
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/* 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* reverse(Node * head)
+    {
+        if(head==NULL || head->next==NULL)
+        {
+            return head;
+        }
+        Node* prev=NULL;
+        Node* curr=head;
+        Node* nxt=NULL;
+        while(curr!=NULL)
+        {
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+           
+            
+        }
+        return prev;
+        
+    }
+    Node* addOne(Node *head) 
+    {
+        // Your Code here
+        // return head of list after adding one
+         head=reverse(head);
+         Node * temp=head;
+         Node* tail=NULL;
+         int sum=0,carry=1;
+         while(carry!=0 && temp!=NULL)
+         {
+             sum=temp->data+carry;
+             carry=sum/10;
+             sum=sum%10;
+             temp->data=sum;
+             tail=temp;
+             temp=temp->next;
+         }
+         if(carry!=0)
+         {
+             tail->next=new Node(1);
+         }
+         head=reverse(head);
+         return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
